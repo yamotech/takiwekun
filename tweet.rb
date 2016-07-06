9c2286b5d112e173d4637a5da56ac36f76410376
yamotech <nothings.2c9@gmail.com>
2016-07-06 23:13:28 +0900

    ACCESS_TOKEN_KEY を ACCESS_TOKENに変更

--- tweet.rb
+++ tweet.rb
@@ -8,7 +8,7 @@ require "twitter"
 # ツイッターのAPIキー
 CONSUMER_KEY = ENV['CONSUMER_KEY'] 
 CONSUMER_SECRET = ENV['CONSUMER_SECRET']
-ACCESS_TOKEN_KEY = ENV['ACCESS_TOKEN_KEY']
+ACCESS_TOKEN = ENV['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = ENV['ACCESS_TOKEN_SECRET']
 
 
@@ -16,10 +16,10 @@ def do_tweet(content)
 	client = Twitter::REST::Client.new do |config|
 		config.consumer_key = CONSUMER_KEY
 		config.consumer_secret = CONSUMER_SECRET
-		config.access_token = ACCESS_TOKEN_KEY
+		config.access_token = ACCESS_TOKEN
 		config.access_token_secret = ACCESS_TOKEN_SECRET
 	end
-
+  
   # ツイート
   client.update(content)
 end
